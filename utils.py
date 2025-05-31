172d8a05b0d42eadad1c46c649db63bbe5938bd9
imperionite <lr.aimperial@mmdc.mcl.edu.ph>
2025-05-31 13:58:14 +0800

    :pencil2: (utils) edit file

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def add_footer():
     """
     footer_html = """
         <div class="footer-text">
-            <a href="https://github.com/imperionite/ghg" target="_blank" rel="noopener noreferrer">GHG Data Explorer PH </a>by<a href="https://github.com/imperionite" target="_blank" rel="noopener noreferrer"> Arnel Imperial</a>. Built with <a href="https://docs.python.org/3/faq/general.html#what-is-python" target="_blank" rel="noopener noreferrer">Python</a> and Streamlit
+            <a href="https://github.com/imperionite/ghg" target="_blank" rel="noopener noreferrer">GHG Data Explorer PH </a>by<a href="https://github.com/imperionite" target="_blank" rel="noopener noreferrer"> Arnel Imperial</a>. Built with <a href="https://www.r-project.org/about.html" target="_blank" rel="noopener noreferrer">R</a>,<a href="https://docs.python.org/3/faq/general.html#what-is-python" target="_blank" rel="noopener noreferrer"> Python</a> and Streamlit
         </div>
     """
     st.markdown(footer_style + footer_html, unsafe_allow_html=True)
